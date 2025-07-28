a3cf6c951ddaf0a8945755505bccb12c6b761719
= <daniell.felipe.2017@gmail.com>
2025-07-28 19:49:31 -0300

    feat: add List component

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,5 +1,6 @@
 import { View } from 'react-native'
 import { HomeHeader } from '@/components/HomeHeader'
+import { List } from '@/components/List'
 import { Target } from '@/components/Target'
 
 const summary = {
@@ -8,14 +9,43 @@ const summary = {
   output: { label: 'Saídas', value: '-R$ 883.65' },
 }
 
-const targets = [{ name: 'Cadeira Ergonômica', percentage: '75%', current: 'R$ 900,00', target: 'R$ 1.200,00' }]
+const targets = [
+  {
+    id: '1',
+    name: 'Apple Watch',
+    percentage: '50%',
+    current: 'R$ 580,00',
+    target: 'R$ 1.700,00',
+  },
+  {
+    id: '2',
+    name: 'Cadeira Ergonômica',
+    percentage: '75%',
+    current: 'R$ 900,00',
+    target: 'R$ 1.200,00',
+  },
+  {
+    id: '3',
+    name: 'Viagem para o Rio de Janeiro',
+    percentage: '75%',
+    current: 'R$ 1.200,00',
+    target: 'R$ 3.000,00',
+  },
+]
 
 export default function Index() {
   return (
     <View style={{ flex: 1 }}>
       <HomeHeader data={summary} />
 
-      <Target data={targets[0]} />
+      <List
+        title="Metas"
+        data={targets}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Target data={item} />}
+        emptyMessage="Nenhuma meta. Toque em nova meta para criar."
+        containerStyle={{ paddingHorizontal: 24 }}
+      />
     </View>
   )
 }
