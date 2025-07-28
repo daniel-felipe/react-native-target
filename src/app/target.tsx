8d24aea357d7113394d021cb423bcccf64d861e6
= <daniell.felipe.2017@gmail.com>
2025-07-28 20:50:38 -0300

    feat: add Input component

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,12 +1,18 @@
-import { router } from 'expo-router'
-import { Button, View } from 'react-native'
+import { View } from 'react-native'
+
+import { Button } from '@/components/Button'
+import { Input } from '@/components/Input'
 import { PageHeader } from '@/components/PageHeader'
 
 export default function Target() {
   return (
     <View style={{ flex: 1, padding: 24 }}>
       <PageHeader title="Meta" subtitle="Economize para alcançar sua meta financeira." />
-      <Button title="Voltar" onPress={() => router.back()} />
+
+      <View style={{ marginTop: 32, gap: 24 }}>
+        <Input label="Nome da meta" placeholder="Ex: Viagem para praia, Apple Watch" />
+        <Button title="Salvar" />
+      </View>
     </View>
   )
 }
