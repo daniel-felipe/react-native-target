15d18401207e99ece655c5747379a87e87606a8f
= <daniell.felipe.2017@gmail.com>
2025-08-05 21:14:34 -0300

    feat: add sqlite and create migration

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,9 @@
 import { Inter_400Regular, Inter_500Medium, Inter_700Bold, useFonts } from '@expo-google-fonts/inter'
 import { Stack } from 'expo-router'
+import { SQLiteProvider } from 'expo-sqlite'
+import { Suspense } from 'react'
 import { Loading } from '@/components/Loading'
+import { migrate } from '@/database/migrate'
 import { colors } from '@/theme/colors'
 
 export default function Layout() {
@@ -15,11 +18,15 @@ export default function Layout() {
   }
 
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-        contentStyle: { backgroundColor: colors.white },
-      }}
-    />
+    <Suspense fallback={<Loading />}>
+      <SQLiteProvider databaseName="target.db" onInit={migrate} useSuspense>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: { backgroundColor: colors.white },
+          }}
+        />
+      </SQLiteProvider>
+    </Suspense>
   )
 }
