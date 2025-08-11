c46cb057258bb0afe80201b3889b3a9760d53eac
= <daniell.felipe.2017@gmail.com>
2025-08-11 19:55:12 -0300

    feat: add use-target-database hook

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,19 +1,63 @@
-import { View } from 'react-native'
-
+import { router, useLocalSearchParams } from 'expo-router'
+import { useState } from 'react'
+import { Alert, View } from 'react-native'
 import { Button } from '@/components/Button'
 import { CurrencyInput } from '@/components/CurrencyInput'
 import { Input } from '@/components/Input'
 import { PageHeader } from '@/components/PageHeader'
+import { useTargetDatabase } from '@/database/use-target-database'
 
 export default function Target() {
+  const [isProcessing, setIsProcessing] = useState(false)
+  const [name, setName] = useState('')
+  const [amount, setAmount] = useState(0)
+
+  const params = useLocalSearchParams<{ id?: string }>()
+  const targetDatabase = useTargetDatabase();
+
+  function handleSave() {
+    if (!name.trim() || amount <= 0) {
+      return Alert.alert('Atenção', 'Preencha nome e valor.')
+    }
+
+    setIsProcessing(true)
+
+    if (params?.id) {
+    } else {
+      create()
+    }
+  }
+
+  async function create() {
+    try {
+      await targetDatabase.create({ name, amount });
+
+      Alert.alert('Nova Meta', 'Meta criada com sucesso!', [
+        {
+          text: 'Ok',
+          onPress: () => router.back(),
+        },
+      ])
+    } catch (error) {
+      Alert.alert('Erro', 'Não foi possível criar a meta.')
+      console.log(error)
+      setIsProcessing(false)
+    }
+  }
+
   return (
     <View style={{ flex: 1, padding: 24 }}>
       <PageHeader title="Meta" subtitle="Economize para alcançar sua meta financeira." />
 
       <View style={{ marginTop: 32, gap: 24 }}>
-        <Input label="Nome da meta" placeholder="Ex: Viagem para praia, Apple Watch" />
-        <CurrencyInput label="Valor alvo (R$)" value={0} />
-        <Button title="Salvar" />
+        <Input
+          label="Nome da meta"
+          placeholder="Ex: Viagem para praia, Apple Watch"
+          value={name}
+          onChangeText={setName}
+        />
+        <CurrencyInput label="Valor alvo (R$)" value={amount} onChangeValue={setAmount} />
+        <Button title="Salvar" isProcessing={isProcessing} onPress={handleSave} />
       </View>
     </View>
   )
