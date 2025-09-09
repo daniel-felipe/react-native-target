fb2c739e428d828884a2d95dc2f593fc095c9c0a
Daniel <daniell.felipe.2017@gmail.com>
2025-09-08 22:12:19 -0300

    feat: display summary values

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,59 +1,84 @@
-import { router, useFocusEffect } from 'expo-router'
-import { useCallback, useState } from 'react'
-import { Alert, StatusBar, View } from 'react-native'
+import { router, useFocusEffect } from "expo-router";
+import { useCallback, useState } from "react";
+import { Alert, StatusBar, View } from "react-native";
 
-import { Button } from '@/components/Button'
-import { HomeHeader } from '@/components/HomeHeader'
-import { List } from '@/components/List'
-import { Loading } from '@/components/Loading'
-import { Target, type TargetProps } from '@/components/Target'
-import { useTargetDatabase } from '@/database/use-target-database'
-import { numberToCurrency } from '@/utils/numberToCurrency'
-
-const summary = {
-  total: 'R$ 2.680,00',
-  input: { label: 'Entradas', value: 'R$ 6,184.90' },
-  output: { label: 'Saídas', value: '-R$ 883.65' },
-}
+import { Button } from "@/components/Button";
+import { HomeHeader, HomeHeaderProps } from "@/components/HomeHeader";
+import { List } from "@/components/List";
+import { Loading } from "@/components/Loading";
+import { Target, type TargetProps } from "@/components/Target";
+import { useTargetDatabase } from "@/database/use-target-database";
+import { numberToCurrency } from "@/utils/numberToCurrency";
+import { useTransactionsDatabase } from "@/database/use-transactions-database";
 
 export default function Index() {
-  const [isFetching, setIsFetching] = useState(true)
-  const [targets, setTargets] = useState<TargetProps[]>([])
-  const targetDatabase = useTargetDatabase()
+  const [summary, setSummary] = useState<HomeHeaderProps>();
+  const [isFetching, setIsFetching] = useState(true);
+  const [targets, setTargets] = useState<TargetProps[]>([]);
+
+  const targetDatabase = useTargetDatabase();
+  const transactionsDatabase = useTransactionsDatabase();
 
   async function fetchTargets(): Promise<TargetProps[]> {
     try {
-      const response = await targetDatabase.listBySavedValue()
+      const response = await targetDatabase.listByClosestTarget();
       return response.map((item) => ({
         id: String(item.id),
         name: item.name,
         current: numberToCurrency(item.current),
         percentage: `${item.percentage.toFixed(0)}%`,
         target: numberToCurrency(item.amount),
-      }))
+      }));
     } catch (error) {
-      Alert.alert('Erro', 'Não foi possível carregar as metas.')
-      console.error(error)
+      Alert.alert("Erro", "Não foi possível carregar as metas.");
+      console.error(error);
+    }
+  }
+
+  async function fetchSummary(): Promise<HomeHeaderProps> {
+    try {
+      const response = await transactionsDatabase.summary();
+
+      return {
+        total: numberToCurrency(response.input + response.output),
+        input: {
+          label: "Entradas",
+          value: numberToCurrency(response.input),
+        },
+        output: {
+          label: "Saídas",
+          value: numberToCurrency(response.output),
+        },
+      };
+    } catch (error) {
+      Alert.alert("Erro", "Não foi possível carregar o resumo.");
+      console.log(error);
     }
   }
 
   async function fetchData() {
-    const targetDataPromise = fetchTargets()
+    const targetDataPromise = fetchTargets();
+    const dataSummaryPromise = fetchSummary();
 
-    const [targetData] = await Promise.all([targetDataPromise])
+    const [targetData, summaryData] = await Promise.all([
+      targetDataPromise,
+      dataSummaryPromise,
+    ]);
 
-    setTargets(targetData)
-    setIsFetching(false)
+    setTargets(targetData);
+    setSummary(summaryData);
+
+    setIsFetching(false);
   }
 
   useFocusEffect(
     useCallback(() => {
-      fetchData()
+      fetchData();
     }, []),
-  )
+  );
 
   if (isFetching) {
-    return <Loading />
+    return <Loading />;
   }
 
   return (
@@ -65,14 +90,19 @@ export default function Index() {
         title="Metas"
         data={targets}
         keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Target data={item} onPress={() => router.navigate(`/in-progress/${item.id}`)} />}
+        renderItem={({ item }) => (
+          <Target
+            data={item}
+            onPress={() => router.navigate(`/in-progress/${item.id}`)}
+          />
+        )}
         emptyMessage="Nenhuma meta. Toque em nova meta para criar."
         containerStyle={{ paddingHorizontal: 24 }}
       />
 
       <View style={{ padding: 24, paddingBottom: 32 }}>
-        <Button title="Nova meta" onPress={() => router.navigate('/target')} />
+        <Button title="Nova meta" onPress={() => router.navigate("/target")} />
       </View>
     </View>
-  )
+  );
 }
